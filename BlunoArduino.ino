504fe3afdb799919ea85d37a7eee59a7baaecea3
bn3101group7 <bn3101.group7@gmail.com>
2014-09-22 22:26:52 +0800

    added ability to read output from uv sensor and output reading once

--- BlunoArduino.ino
+++ BlunoArduino.ino
@@ -5,8 +5,16 @@ char array[2];
 String out = "";
 String skin = "";
 String gender = "";
+
+int UVOUT = A0; //Output from the sensor
+int REF_3V3 = A1; //3.3V power on the Arduino board
+int uvIndex;
+
 void setup() {
-    Serial.begin(115200);               //initial the Serial
+  Serial.begin(115200);               //initial the Serial
+  
+  pinMode(UVOUT, INPUT);
+  pinMode(REF_3V3, INPUT);
 }
 
 int charToInt(char c) {
@@ -36,41 +44,55 @@ int charToInt(char c) {
 
 void loop()
 {
-    if(Serial.available())
-    {
-      Serial.readBytes(array,len);
-      code1 = charToInt(array[0]);
-      switch(code1) {
-        case 1:
-          skin = "Fair";
-          break;
-        case 2:
-          skin = "Tanned";
-          break;
-        case 3:
-          skin = "Very Tanned";
-          break;
-        default:
-          Serial.write("nothing");
-          break;
-      }
-      code2 = charToInt(array[1]);
-      switch(code2) {
-        case 1:
-          gender = "Male";
-          break;
-        case 2:
-          gender = "Female";
-          break;
-        default:
-          break;
-      }
-      Serial.print("Skin is "+skin+" Gender is "+gender+"\n");
-      //Serial.print(out);
-      //out="";
-      //Serial.write(code);    //send what has been received
+  uvIndex = -1;
+  if(Serial.available())
+  {
+    int uvLevel = averageAnalogRead(UVOUT);
+    int refLevel = averageAnalogRead(REF_3V3);
+    float outputVoltage = 3.3 / refLevel * uvLevel;
+    Serial.readBytes(array,len);
+    code1 = charToInt(array[0]);
+    switch(code1) {
+      case 1:
+        skin = "Fair";
+        uvIndex = (outputVoltage - 1)*12.5+0.5;
+        break;
+      case 2:
+        skin = "Tanned";
+        break;
+      case 3:
+        skin = "Very Tanned";
+        break;
+      default:
+        Serial.write("nothing");
+        break;
     }
+    code2 = charToInt(array[1]);
+    switch(code2) {
+      case 1:
+        gender = "Male";
+        break;
+      case 2:
+        gender = "Female";
+        break;
+      default:
+        break;
+    }
+    Serial.print("Skin is "+skin+" Gender is "+gender+" UV Index is "+String(uvIndex)+"\n");
+    //Serial.print(out);
+    //out="";
+    //Serial.write(code);    //send what has been received
+  }
 }
 
+int averageAnalogRead(int pinToRead)
+{
+  byte numberOfReadings = 16;
+  unsigned int runningValue = 0; 
 
+  for(int x = 0 ; x < numberOfReadings ; x++)
+    runningValue += analogRead(pinToRead);
+  runningValue /= numberOfReadings;
 
+  return(runningValue);  
+}
