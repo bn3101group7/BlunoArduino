0eb020056411094649d775815f7382e1d668aeb8
bn3101group7 <bn3101.group7@gmail.com>
2014-09-29 00:40:29 +0800

    updated more scenarios

--- BlunoArduino.ino
+++ BlunoArduino.ino
@@ -1,10 +1,14 @@
 int code1=0; //currently, code1 for skin, code2 for gender
 int code2=0;
-int len = 2;
-char array[2];
+int code3=0;
+int code4=0;
+int len = 4;
+char array[4];
 String out = "";
 String skin = "";
-String gender = "";
+String eye = "";
+String hair = "";
+String frec = "";
 String uvTime = "";
 
 int UVOUT = A0; //Output from the sensor
@@ -58,34 +62,106 @@ void loop()
       //take in age group and gender as well
       //output recommended spf value and duration
       case 1:
-        skin = "Fair";
+        skin = "Very Fair";
         uvIndex = (outputVoltage - 1)*12.5+0.5;
         uvTime = "10";
         break;
       case 2:
-        skin = "Tanned";
+        skin = "Fair";
+        uvIndex = (outputVoltage - 1)*12.5+0.5;
+        uvTime = "10";
         break;
       case 3:
+        skin = "Normal";
+        uvIndex = (outputVoltage - 1)*12.5+0.5;
+        uvTime = "10";
+        break;
+      case 4:
+        skin = "Tanned";
+        uvIndex = (outputVoltage - 1)*12.5+0.5;
+        uvTime = "10";
+        break;
+      case 5:
         skin = "Very Tanned";
+        uvIndex = (outputVoltage - 1)*12.5+0.5;
+        uvTime = "10";
+        break;
+      case 6:
+        skin = "Very Very Tanned";
+        uvIndex = (outputVoltage - 1)*12.5+0.5;
+        uvTime = "10";
         break;
       default:
-        Serial.write("nothing");
+        skin = "Nothing";
         break;
     }
     code2 = charToInt(array[1]);
     switch(code2) {
       case 1:
-        gender = "Male";
+        eye = "Light blue, gray or green";
         break;
       case 2:
-        gender = "Female";
+        eye = "Blue, gray or green";
+        break;
+      case 3:
+        eye = "Dark blue, gray or green";
+        break;
+      case 4:
+        eye = "Brown";
+        break;
+      case 5:
+        eye = "Black";
         break;
       default:
+        eye = "Nothing";
         break;
     }
-    Serial.print("Skin is "+skin+" Gender is "+
-    gender+" UV Index is "+String(uvIndex)+
-    " Recommended UV exposure time is "+uvTime+
+    code3 = charToInt(array[2]);
+    switch(code3) {
+      case 1:
+        hair = "Sandy red";
+        break;
+      case 2:
+        hair = "Blond";
+        break;
+      case 3:
+        hair = "Chestnut or dark blond";
+        break;
+      case 4:
+        hair = "Brown";
+        break;
+      case 5:
+        hair = "Black";
+        break;
+      default:
+        hair = "Nothing";
+        break;
+    }
+    code4 = charToInt(array[3]);
+    switch(code4) {
+      case 1:
+        frec = "Many";
+        break;
+      case 2:
+        frec = "Several";
+        break;
+      case 3:
+        frec = "Few";
+        break;
+      case 4:
+        frec = "Rare";
+        break;
+      case 5:
+        frec = "None";
+        break;
+      default:
+        frec = "Nothing";
+        break;
+    }
+    Serial.print("Skin is "+skin+"\nEye is "+
+    eye+"\nHair is "+hair+"\nNumber of freckles is "+
+    frec+"\nUV Index is "+String(uvIndex)+
+    "\nRecommended UV exposure time is "+uvTime+
     " minutes."+"\n");
     //Serial.print(out);
     //out="";
