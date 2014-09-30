9c51377977163cf516bc189e5644ffb8f2771c3e
bn3101group7 <bn3101.group7@gmail.com>
2014-09-30 23:38:51 +0800

    updated code to calculate skin type based on Fitzpatrick

--- BlunoArduino.ino
+++ BlunoArduino.ino
@@ -1,13 +1,9 @@
-int code1=0; //currently, code1 for skin, code2 for gender
-int code2=0;
-int code3=0;
-int code4=0;
-int code5=0;
-int code6=0;
-int code7=0;
-int code8=0;
-int len = 8;
-char array[8];
+int code[10];
+int value[10];
+int len = 10;
+char array[10];
+int score = 0;
+String skinType = "";
 String out = "";
 String skin = "";
 String eye = "";
@@ -17,6 +13,8 @@ String burn = "";
 String brownFreq = "";
 String brownInt = "";
 String face = "";
+String tanFreq = "";
+String tanHist = "";
 String uvTime = "";
 
 int UVOUT = A0; //Output from the sensor
@@ -28,6 +26,10 @@ void setup() {
   
   pinMode(UVOUT, INPUT);
   pinMode(REF_3V3, INPUT);
+  
+  for(int i=0; i<len; i++) {
+    code[i] = 0;
+  }  
 }
 
 int charToInt(char c) {
@@ -58,207 +60,331 @@ int charToInt(char c) {
 void loop()
 {
   uvIndex = -1;
+  score = 0;
   if(Serial.available())
   {
     int uvLevel = averageAnalogRead(UVOUT);
     int refLevel = averageAnalogRead(REF_3V3);
     float outputVoltage = 3.3 / refLevel * uvLevel;
     Serial.readBytes(array,len);
-    code1 = charToInt(array[0]);
-    switch(code1) {
+    for(int j=0; j<len; j++) {
+      code[j] = charToInt(array[j]);
+    }
+    switch(code[0]) {
       case 1:
+        eye = "0";
+        value[0]=0;
+        break;
+      case 2:
         eye = "1";
+        value[0]=1;
         break;
-      case 2:
+      case 3:
         eye = "2";
-        break;
-      case 3:
-        eye = "3";
+        value[0]=2;
         break;
       case 4:
-        eye = "4";
+        eye = "3";
+        value[0]=3;
         break;
       case 5:
-        eye = "5";
+        eye = "4";
+        value[0]=4;
         break;
       default:
-        eye = "6";
+        eye = "-1";
+        value[0]=-99;
         break;
     }
-    code2 = charToInt(array[1]);
-    switch(code2) {
+    switch(code[1]) {
       case 1:
-        hair = "1";
+        hair = "0";
+        value[1]=0;
         break;
       case 2:
-        hair = "2";
+        hair = "1";
+        value[1]=1;
         break;
       case 3:
-        hair = "3";
+        hair = "2";
+        value[1]=2;
         break;
       case 4:
-        hair = "4";
+        hair = "3";
+        value[1]=3;
         break;
       case 5:
-        hair = "5";
+        hair = "4";
+        value[1]=4;
         break;
       default:
-        hair = "6";
+        hair = "-1";
+        value[1]=-99;
         break;
     }
-    code3 = charToInt(array[2]);
-    switch(code3) {
+    switch(code[2]) {
       //take in code1, ask more switch statements for hair color, eye color and tanning ability
       //take in age group and gender as well
       //output recommended spf value and duration
       case 1:
-        skin = "1";
+        skin = "0";
+        value[2]=0;
         uvIndex = (outputVoltage - 1)*12.5+0.5;
         uvTime = "10";
         break;
       case 2:
-        skin = "2";
+        skin = "1";
+        value[2]=1;
         uvIndex = (outputVoltage - 1)*12.5+0.5;
         uvTime = "10";
         break;
       case 3:
-        skin = "3";
+        skin = "2";
+        value[2]=2;
         uvIndex = (outputVoltage - 1)*12.5+0.5;
         uvTime = "10";
         break;
       case 4:
-        skin = "4";
+        skin = "2";
+        value[2]=2;
         uvIndex = (outputVoltage - 1)*12.5+0.5;
         uvTime = "10";
         break;
       case 5:
-        skin = "5";
+        skin = "3";
+        value[2]=3;
         uvIndex = (outputVoltage - 1)*12.5+0.5;
         uvTime = "10";
         break;
       case 6:
-        skin = "6";
+        skin = "4";
+        value[2]=4;
         uvIndex = (outputVoltage - 1)*12.5+0.5;
         uvTime = "10";
         break;
       default:
-        skin = "99";
+        skin = "-99";
+        value[2]=-99;
         break;
     }
-    code4 = charToInt(array[3]);
-    switch(code4) {
+    switch(code[3]) {
       case 1:
-        frec = "1";
+        frec = "0";
+        value[3]=0;
         break;
       case 2:
-        frec = "2";
+        frec = "1";
+        value[3]=1;
         break;
       case 3:
-        frec = "3";
+        frec = "2";
+        value[3]=2;
         break;
       case 4:
-        frec = "4";
+        frec = "3";
+        value[3]=3;
         break;
       case 5:
-        frec = "5";
+        frec = "4";
+        value[3]=4;
         break;
       default:
         frec = "99";
+        value[3]=-99;
         break;
     }
-    code5 = charToInt(array[4]);
-    switch(code5) {
+    switch(code[4]) {
       case 1:
-        burn = "1";
+        burn = "0";
+        value[4]=0;
         break;
       case 2:
-        burn = "2";
+        burn = "1";
+        value[4]=1;
         break;
       case 3:
-        burn = "3";
+        burn = "2";
+        value[4]=2;
         break;
       case 4:
-        burn = "4";
+        burn = "3";
+        value[4]=3;
         break;
       case 5:
-        burn = "5";
+        burn = "4";
+        value[4]=4;
         break;
       default:
         burn = "99";
+        value[4]=-99;
         break;
     }
-    code6 = charToInt(array[5]);
-    switch(code6) {
+    switch(code[5]) {
       case 1:
-        brownFreq = "1";
+        brownFreq = "0";
+        value[5]=0;
         break;
       case 2:
-        brownFreq = "2";
+        brownFreq = "1";
+        value[5]=1;
         break;
       case 3:
-        brownFreq = "3";
+        brownFreq = "2";
+        value[5]=2;
         break;
       case 4:
-        brownFreq = "4";
+        brownFreq = "3";
+        value[5]=3;
         break;
       case 5:
-        brownFreq = "5";
+        brownFreq = "4";
+        value[5]=4;
         break;
       default:
         brownFreq = "99";
+        value[5]=-99;
         break;
     }
-    code7 = charToInt(array[6]);
-    switch(code7) {
+    switch(code[6]) {
       case 1:
-        brownInt = "1";
+        brownInt = "0";
+        value[6]=0;
         break;
       case 2:
-        brownInt = "2";
+        brownInt = "1";
+        value[6]=1;
         break;
       case 3:
-        brownInt = "3";
+        brownInt = "2";
+        value[6]=2;
         break;
       case 4:
-        brownInt = "4";
+        brownInt = "3";
+        value[6]=3;
         break;
       case 5:
-        brownInt = "5";
+        brownInt = "4";
+        value[6]=4;
         break;
       default:
         brownInt = "99";
+        value[6]=-99;
         break;
     }
-    code8 = charToInt(array[7]);
-    switch(code8) {
+    switch(code[7]) {
       case 1:
-        face = "1";
+        face = "0";
+        value[7]=0;
         break;
       case 2:
-        face = "2";
+        face = "1";
+        value[7]=1;
         break;
       case 3:
-        face = "3";
+        face = "2";
+        value[7]=2;
         break;
       case 4:
-        face = "4";
+        face = "3";
+        value[7]=3;
         break;
       case 5:
-        face = "5";
+        face = "4";
+        value[7]=4;
         break;
       default:
         face = "99";
+        value[7]=-99;
         break;
     }
-    Serial.print("Skin "+skin+
-    "\nEye "+eye+
+    switch(code[8]) {
+      case 1:
+        tanFreq = "0";
+        value[8]=0;
+        break;
+      case 2:
+        tanFreq = "1";
+        value[8]=1;
+        break;
+      case 3:
+        tanFreq = "2";
+        value[8]=2;
+        break;
+      case 4:
+        tanFreq = "3";
+        value[8]=3;
+        break;
+      case 5:
+        tanFreq = "4";
+        value[8]=4;
+        break;
+      default:
+        tanFreq = "99";
+        value[8]=-99;
+        break;
+    }
+    switch(code[9]) {
+      case 1:
+        tanHist = "0";
+        value[9]=0;
+        break;
+      case 2:
+        tanHist = "1";
+        value[9]=1;
+        break;
+      case 3:
+        tanHist = "2";
+        value[9]=2;
+        break;
+      case 4:
+        tanHist = "3";
+        value[9]=3;
+        break;
+      case 5:
+        tanHist = "4";
+        value[9]=4;
+        break;
+      default:
+        tanHist = "99";
+        value[9]=-99;
+        break;
+    }
+    for(int k=0; k<len; k++) {
+      score+=value[k];
+    }
+
+    if(score>-1) {
+      skinType = "I";
+    }
+    if(score>6) {
+      skinType = "II";
+    }
+    if(score>13) {
+      skinType = "III";
+    }
+    if(score>20) {
+      skinType = "IV";
+    }
+    if(score>27) {
+      skinType = "V";
+    }
+    if(score>34) {
+      skinType = "VI";
+    }
+    
+    Serial.print("Eye "+eye+
     "\nHair "+hair+
-    "\nFreckles "+frec+
+    "\nSkin "+skin+
+    "\nFrec "+frec+
     "\nBurn "+burn+
-    "\nBrown Freq "+brownFreq+
-    "\nBrown Int "+brownInt+
+    "\nBrownFreq "+brownFreq+
+    "\nBrownInt "+brownInt+
     "\nFace "+face+
-    "\nUV Index "+String(uvIndex)+
+    "\nTanFreq "+tanFreq+
+    "\nTanHist "+tanHist+
+    "\nScore "+String(score)+
+    "\nType "+skinType+
+    "\nUVI "+String(uvIndex)+
     "\nRecommended UV exposure time is "+uvTime+" minutes."+"\n");
     //Serial.print(out);
     //out="";
